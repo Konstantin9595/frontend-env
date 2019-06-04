bcba1b75df8d8f719cb5bd590cf0f4d18097d4b7
Konstantin <konstantins9595@gmail.com>
2019-06-04 15:46:09 +0600

    recreate react project

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App/App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="header">
+        Hello, User
+      </header>
+    </div>
+  );
+}
+
+export default App;
